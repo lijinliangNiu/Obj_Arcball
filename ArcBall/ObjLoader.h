9189be30623e330b4d856f095c510891abe64e89
Lijinliang Niu <niulijinliang000@gmail.com>
2020-04-12 13:51:45 +0800

    use glm

--- ArcBall/ObjLoader.h
+++ ArcBall/ObjLoader.h
@@ -6,22 +6,23 @@
 #include <fstream>
 #include <math.h>
 
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/vector_angle.hpp>
+#include <glm/gtx/projection.hpp>
 
 #define OBJL_CONSOLE_OUTPUT
 
-namespace objl{
-    struct Vertex{
+namespace objl {
+    struct Vertex {
         glm::vec3 Position;
         glm::vec3 Normal;
         glm::vec2 TextureCoordinate;
     };
 
-    struct Material
-    {
-        Material()
-        {
+    struct Material {
+        Material() {
             name;
             Ns = 0.0f;
             Ni = 0.0f;
@@ -29,7 +30,6 @@ namespace objl{
             illum = 0;
         }
 
-        // Material Name
         std::string name;
         // Ambient Color
         glm::vec3 Ka;
@@ -59,85 +59,34 @@ namespace objl{
         std::string map_bump;
     };
 
-    struct Mesh{
-        Mesh(){
+    struct Mesh {
+        Mesh() {
         }
         Mesh(std::vector<Vertex>& _Vertices, std::vector<unsigned int>& _Indices) {
             Vertices = _Vertices;
             Indices = _Indices;
         }
-        // Mesh Name
         std::string MeshName;
-        // Vertex List
         std::vector<Vertex> Vertices;
-        // Index List
         std::vector<unsigned int> Indices;
 
-        // Material
         Material MeshMaterial;
     };
 
-    // Namespace: Math
-    //
-    // Description: The namespace that holds all of the math
-    //	functions need for OBJL
-    namespace math
-    {
-        // glm::vec3 Magnitude Calculation
-        float MagnitudeV3(const glm::vec3 in)
-        {
-            return (sqrtf(powf(in.x, 2) + powf(in.y, 2) + powf(in.z, 2)));
-        }
-
-        // glm::vec3 DotProduct
-        float DotV3(const glm::vec3 a, const glm::vec3 b)
-        {
-            return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
-        }
-
-        // Angle between 2 glm::vec3 Objects
-        float AngleBetweenV3(const glm::vec3 a, const glm::vec3 b)
-        {
-            float angle = DotV3(a, b);
-            angle /= (MagnitudeV3(a) * MagnitudeV3(b));
-            return angle = acosf(angle);
-        }
-
-        // Projection Calculation of a onto b
-        glm::vec3 ProjV3(const glm::vec3 a, const glm::vec3 b)
-        {
-            glm::vec3 bn = b / MagnitudeV3(b);
-            return bn * DotV3(a, bn);
-        }
-    }
-
-    // Namespace: Algorithm
-    //
-    // Description: The namespace that holds all of the
-    // Algorithms needed for OBJL
-    namespace algorithm
-    {
-        // glm::vec3 Multiplication Opertor Overload
-        glm::vec3 operator*(const float& left, const glm::vec3& right)
-        {
-            return glm::vec3(right.x * left, right.y * left, right.z * left);
-        }
-
+    namespace algorithm {
         // A test to see if P1 is on the same side as P2 of a line segment ab
-        bool SameSide(glm::vec3 p1, glm::vec3 p2, glm::vec3 a, glm::vec3 b)
-        {
+        bool SameSide(glm::vec3 p1, glm::vec3 p2, glm::vec3 a, glm::vec3 b) {
             glm::vec3 cp1 = glm::cross(b - a, p1 - a);
             glm::vec3 cp2 = glm::cross(b - a, p2 - a);
 
-            if (math::DotV3(cp1, cp2) >= 0)
+            if (glm::dot(cp1, cp2) >= 0)
                 return true;
             else
                 return false;
         }
 
         // Generate a cross produect normal for a triangle
-        glm::vec3 GenTriNormal(glm::vec3 t1, glm::vec3 t2, glm::vec3 t3)
-        {
+        glm::vec3 GenTriNormal(glm::vec3 t1, glm::vec3 t2, glm::vec3 t3) {
             glm::vec3 u = t2 - t1;
             glm::vec3 v = t3 - t1;
 
@@ -147,8 +96,7 @@ namespace objl{
         }
 
         // Check to see if a glm::vec3 Point is within a 3 glm::vec3 Triangle
-        bool inTriangle(glm::vec3 point, glm::vec3 tri1, glm::vec3 tri2, glm::vec3 tri3)
-        {
+        bool inTriangle(glm::vec3 point, glm::vec3 tri1, glm::vec3 tri2, glm::vec3 tri3) {
             // Test to see if it is within an infinite prism that the triangle outlines.
             bool within_tri_prisim = SameSide(point, tri1, tri2, tri3) && SameSide(point, tri2, tri1, tri3)
                 && SameSide(point, tri3, tri1, tri2);
@@ -161,11 +109,11 @@ namespace objl{
             glm::vec3 n = GenTriNormal(tri1, tri2, tri3);
 
             // Project the point onto this normal
-            glm::vec3 proj = math::ProjV3(point, n);
+            glm::vec3 proj = glm::proj(point, glm::normalize(n));
 
             // If the distance from the triangle to the point is 0
             //	it lies on the triangle
-            if (math::MagnitudeV3(proj) == 0)
+            if (glm::length(proj) == 0)
                 return true;
             else
                 return false;
@@ -174,73 +122,59 @@ namespace objl{
         // Split a String into a string array at a given token
         inline void split(const std::string &in,
             std::vector<std::string> &out,
-            std::string token)
-        {
+            std::string token) {
             out.clear();
 
             std::string temp;
 
-            for (int i = 0; i < int(in.size()); i++)
-            {
+            for (int i = 0; i < int(in.size()); i++){
                 std::string test = in.substr(i, token.size());
 
-                if (test == token)
-                {
-                    if (!temp.empty())
-                    {
+                if (test == token){
+                    if (!temp.empty()){
                         out.push_back(temp);
                         temp.clear();
                         i += (int)token.size() - 1;
                     }
-                    else
-                    {
+                    else{
                         out.push_back("");
                     }
                 }
-                else if (i + token.size() >= in.size())
-                {
+                else if (i + token.size() >= in.size()){
                     temp += in.substr(i, token.size());
                     out.push_back(temp);
                     break;
                 }
-                else
-                {
+                else{
                     temp += in[i];
                 }
             }
         }
 
         // Get tail of string after first token and possibly following spaces
-        inline std::string tail(const std::string &in)
-        {
+        inline std::string tail(const std::string &in){
             size_t token_start = in.find_first_not_of(" \t");
             size_t space_start = in.find_first_of(" \t", token_start);
             size_t tail_start = in.find_first_not_of(" \t", space_start);
             size_t tail_end = in.find_last_not_of(" \t");
-            if (tail_start != std::string::npos && tail_end != std::string::npos)
-            {
+            if (tail_start != std::string::npos && tail_end != std::string::npos){
                 return in.substr(tail_start, tail_end - tail_start + 1);
             }
-            else if (tail_start != std::string::npos)
-            {
+            else if (tail_start != std::string::npos){
                 return in.substr(tail_start);
             }
             return "";
         }
 
         // Get first token of string
-        inline std::string firstToken(const std::string &in)
-        {
-            if (!in.empty())
-            {
+        inline std::string firstToken(const std::string &in){
+            if (!in.empty()){
                 size_t token_start = in.find_first_not_of(" \t");
                 size_t token_end = in.find_first_of(" \t", token_start);
-                if (token_start != std::string::npos && token_end != std::string::npos)
-                {
+                if (token_start != std::string::npos && token_end != std::string::npos){
                     return in.substr(token_start, token_end - token_start);
                 }
-                else if (token_start != std::string::npos)
-                {
+                else if (token_start != std::string::npos){
                     return in.substr(token_start);
                 }
             }
@@ -249,8 +183,7 @@ namespace objl{
 
         // Get element at given index position
         template <class T>
-        inline const T & getElement(const std::vector<T> &elements, std::string &index)
-        {
+        inline const T & getElement(const std::vector<T> &elements, std::string &index){
             int idx = std::stoi(index);
             if (idx < 0)
                 idx = int(elements.size()) + idx;
@@ -260,28 +193,14 @@ namespace objl{
         }
     }
 
-    // Class: Loader
-    //
-    // Description: The OBJ Model Loader
-    class Loader
-    {
+    class Loader{
     public:
-        // Default Constructor
-        Loader()
-        {
-
+        Loader(){
         }
-        ~Loader()
-        {
+        ~Loader(){
             LoadedMeshes.clear();
         }
 
-        // Load a file into the loader
-        //
-        // If file is loaded return true
-        //
-        // If the file is unable to be found
-        // or unable to be loaded return false
         bool LoadFile(std::string Path)
         {
             // If the file is not an .obj file return false
@@ -800,7 +719,7 @@ namespace objl{
                     }
 
                     // If Vertex is not an interior vertex
-                    float angle = math::AngleBetweenV3(pPrev.Position - pCur.Position, pNext.Position - pCur.Position) * (180 / 3.14159265359);
+                    float angle = glm::degrees(glm::angle(glm::normalize(pPrev.Position - pCur.Position), glm::normalize(pNext.Position - pCur.Position)));
                     if (angle <= 0 && angle >= 180)
                         continue;
 
